8b67abfdfa3ae3c55b51e5924d766ca33d333956
Václav Uher <vaclav.uher@gmail.com>
2018-06-17 17:51:48 +0200

    crawler work in progress

--- src/http_client/app/headless_browsers.py
+++ src/http_client/app/headless_browsers.py
@@ -10,13 +10,14 @@ def chrome_driver(window_width=1200, window_height=600):
     :return: Loaded Chrome driver.
     """
     act_dir = os.path.dirname(__file__)
-    data_dir = join(str(Path(act_dir).parent.parent), 'data')
 
     options = webdriver.ChromeOptions()
     # do not show window
     options.add_argument('headless')
+    # disable GPU
+    options.add_argument('disable-gpu')
     # page view window size
     options.add_argument('window-size=' + str(window_width) + 'x' + str(window_height))
 
-    driver = webdriver.Chrome(join(data_dir, 'chromedriver'), chrome_options=options)
+    driver = webdriver.Chrome(join(act_dir, 'chromedriver'), chrome_options=options)
     return driver
